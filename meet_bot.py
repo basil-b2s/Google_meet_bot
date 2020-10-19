f633f5af6b382c27d9ce9a2843a7f70aeda25c99
Basil Saji <62460730+basil-b2s@users.noreply.github.com>
2020-10-20 01:26:26 +0530

    Update meet_bot.py

--- meet_bot.py
+++ meet_bot.py
@@ -1,5 +1,4 @@
 
-
 # google meet bot
 
 
@@ -8,7 +7,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
-
+import playsound
+import os
+from gtts import gTTS
 
 
 ####################################################
@@ -22,35 +23,24 @@ class Meetbot:
         self.username = username
         self.password = password
         self.meetlink = meetlink
+        self.number_of_stud = 60
+        self.left_limit = 30
 
-        # notification settings (camera , mic)
-        self.opt = Options()
-        self.opt.add_argument("--disable-infobars")
-        self.opt.add_argument("start-maximized")
-        self.opt.add_argument("--disable-extensions")
-        
-        # Pass the argument 1 to allow and 2 to block
-        
-        self.opt.add_experimental_option("prefs", { \
-                "profile.default_content_setting_values.media_stream_mic": 1, 
-                "profile.default_content_setting_values.media_stream_camera": 1,
-                "profile.default_content_setting_values.notifications": 2
-              })
-
-        self.driver = webdriver.Chrome(chrome_options=self.opt, executable_path='.\Chromedriver\chromedriver')
+        self.driver = webdriver.Firefox()
 
         self.login_gmail()
 
         
-        # login into the gmail account through stackoverflow ( since google does't allow automated webrowser login directly )
+        # login into the gmail account 
         
     def login_gmail(self):
 
-        # getting into the google login page through stackoverflow 
+        # getting into the google login page
 
-        self.driver.get("https://accounts.google.com/o/oauth2/auth/identifier?client_id=717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com&scope=profile%20email&redirect_uri=https%3A%2F%2Fstackauth.com%2Fauth%2Foauth2%2Fgoogle&state=%7B%22sid%22%3A1%2C%22st%22%3A%2259%3A3%3Abbc%2C16%3A1125b5ebe8db8eeb%2C10%3A1601450831%2C16%3A5584e909d121ad66%2C0845646c5915346ad7f918f93b9e0ff2cf02cb510d1080b3d27f579f1ab31c93%22%2C%22cdl%22%3Anull%2C%22cid%22%3A%22717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com%22%2C%22k%22%3A%22Google%22%2C%22ses%22%3A%225c2ac33309a6465f9783bd731a8f42ab%22%7D&response_type=code&flowName=GeneralOAuthFlow")
         time.sleep(2)
-
+        
+        self.driver.get('https://accounts.google.com/signin/v2/identifier?flowName=GlifWebSignIn&flowEntry=ServiceLogin')
+        time.sleep(2)        
         # passing the email id
         
         self.driver.find_element_by_xpath('//input[@type="email"]').send_keys(self.username)
@@ -85,14 +75,14 @@ class Meetbot:
         # turning off the mic
 
         mic_off = self.driver.find_element_by_xpath(
-                "/html/body/div[1]/c-wiz/div/div/div[5]/div[3]/div/div/div[2]/div/div/div[1]/div/div[4]/div[1]/div/div/div")
+                "/html/body/div[1]/c-wiz/div/div/div[5]/div[3]/div/div/div[2]/div/div/div[1]/div/div[3]/div[1]/div/div/div")
         mic_off.click()
         time.sleep(2)
 
         # turning off the camera
         
         cam_off = self.driver.find_element_by_xpath(
-                "/html/body/div[1]/c-wiz/div/div/div[5]/div[3]/div/div/div[2]/div/div/div[1]/div/div[4]/div[2]/div/div")
+                "/html/body/div[1]/c-wiz/div/div/div[5]/div[3]/div/div/div[2]/div/div/div[1]/div/div[3]/div[2]/div/div")
         cam_off.click()
         time.sleep(2)
 
@@ -118,11 +108,17 @@ class Meetbot:
 
                     time.sleep(4)
                     people_number = self.driver.find_element_by_xpath("//*[contains(@class,'wnPUne N0PJ8e')]").text
-                    people_number = int(people_number)
-                    print(people_number)
+                    people_number = int(people_number)                    
         
                 except:
-                    pass
+                    
+                    try:
+                        people_number = driver.find_element_by_class_name("rua5Nb").text
+                        people_number = int(people_number[1:-1])
+                        print(people_number)
+           
+                    except:
+                        pass
 
                 if people_number <=30:
                     break
@@ -130,8 +126,10 @@ class Meetbot:
                 else:
                     pass
 
+                print(people_number)
 
-        time.sleep(3)
+
+        time.sleep(1)
 
         self.driver.close()
 
@@ -143,14 +141,28 @@ class Meetbot:
 
 if __name__ == "__main__":
 
-    username = input("Enter Your Mail : ")
+    def voice(txt):
+        speech=gTTS(text=txt,lang='en',slow=False)
+        speech.save("a.mp3")
+        playsound.playsound("a.mp3")
+        os.remove("a.mp3")
+                
+    voice("Enter Your Mail")
+    username = input("Enter Your E-mail : ")
+    voice("Enter the password")
     password = input("Enter the password: ")
-    meetlink = input("Enter the class link: ")
+    voice("Enter the class link")
+    meetlink = input("Enter the google meet class link: ")
 
     meet_bot = Meetbot(username, password, meetlink)
+
+    voice("This session is over")
+    print("This session is Over")
+    
+
+    
             
 
             
 
 #################################################################################
-
